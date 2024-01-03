96ac3a6898c020839a5b7e3bae6152c96f367550
shareBeautiful <1454167941@qq.com>
2024-01-03 21:38:37 +0800

    audio

--- run.py
+++ run.py
@@ -2,12 +2,13 @@ from pathlib import Path
 import shutil
 def run():
     dir_path = Path('./源文件')
-    i = 1
+    orgin_path = Path('./道德经浅解')
+    count = len(list(orgin_path.glob('*.m4a')))+1
     for item in dir_path.glob('*.m4a'):
-        new_path = Path(f'./道德经浅解/{i}.m4a')
+        new_path = Path(f'./道德经浅解/{count}.m4a')
         shutil.copy(item, new_path)
         print(f'复制{item}成功')
-        i+=1
+        count+=1
         
 
 if __name__ == '__main__':
